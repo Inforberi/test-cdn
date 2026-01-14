03e64acbebaaceb187eff70250ba4cd5adaf1267
Алексей <tatarchuk.a.s.1997@gmail.com>
2026-01-14 11:36:37 +0300

    fix: подключил обратно cdn

--- lib/image-url.ts
+++ lib/image-url.ts
@@ -1,17 +1,16 @@
 const CDN = process.env.NEXT_PUBLIC_CDN_URL || 'https://cdn.inforberi.ru';
 
 export function toCdnUrl(src?: string | null) {
-    return src || '';
-    // if (!src) return '';
-    // // если уже абсолютная ссылка — не трогаем
-    // if (
-    //     src.startsWith('http://') ||
-    //     src.startsWith('https://') ||
-    //     src.startsWith('data:')
-    // )
-    //     return src;
-    // // если относительная "/..."
-    // if (src.startsWith('/')) return `${CDN}${src}`;
-    // // если вдруг без "/" (редко)
-    // return `${CDN}/${src}`;
+    if (!src) return '';
+    // если уже абсолютная ссылка — не трогаем
+    if (
+        src.startsWith('http://') ||
+        src.startsWith('https://') ||
+        src.startsWith('data:')
+    )
+        return src;
+    // если относительная "/..."
+    if (src.startsWith('/')) return `${CDN}${src}`;
+    // если вдруг без "/" (редко)
+    return `${CDN}/${src}`;
 }
