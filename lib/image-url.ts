033a041639a0ecdc91d88b519dbd30bca3efa2f3
Алексей <tatarchuk.a.s.1997@gmail.com>
2026-01-14 10:35:20 +0300

    fix: убрал cdn src

--- lib/image-url.ts
+++ lib/image-url.ts
@@ -1,16 +1,17 @@
 const CDN = process.env.NEXT_PUBLIC_CDN_URL || 'https://cdn.inforberi.ru';
 
 export function toCdnUrl(src?: string | null) {
-    if (!src) return '';
-    // если уже абсолютная ссылка — не трогаем
-    if (
-        src.startsWith('http://') ||
-        src.startsWith('https://') ||
-        src.startsWith('data:')
-    )
-        return src;
-    // если относительная "/..."
-    if (src.startsWith('/')) return `${CDN}${src}`;
-    // если вдруг без "/" (редко)
-    return `${CDN}/${src}`;
+    return src || '';
+    // if (!src) return '';
+    // // если уже абсолютная ссылка — не трогаем
+    // if (
+    //     src.startsWith('http://') ||
+    //     src.startsWith('https://') ||
+    //     src.startsWith('data:')
+    // )
+    //     return src;
+    // // если относительная "/..."
+    // if (src.startsWith('/')) return `${CDN}${src}`;
+    // // если вдруг без "/" (редко)
+    // return `${CDN}/${src}`;
 }
